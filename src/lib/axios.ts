6215bdf728de6cf5611de04733891305aaab5471
Gabriel Silveira <ramos97033@outlook.com>
2025-10-22 12:41:03 -0300

    fix: url fix

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "https://72.60.5.169:8000/api/v1",
+   baseURL: "http://72.60.5.169:8000/api/v1",
    headers: {
       "Content-Type": "application/json",
    },
