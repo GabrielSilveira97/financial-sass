eeac134033db8d82991bd4d4f057ab3d94964a54
Gabriel Silveira <ramos97033@outlook.com>
2025-10-22 12:37:58 -0300

    fix: url fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import LoginForm from "@/components/common/LoginForm";
 export default function Home() {
    return (
       <main className="grid grid-cols-1 lg:grid-cols-2 justify-center items-center">
-         <section className="lg:flex justify-center items-center h-screen hidden bg-slate-100">
-            <h1 className="font-bold text-4xl">Financial Logo</h1>
+         <section className="lg:flex justify-center items-center h-screen hidden bg-muted">
+            <h1 className="font-bold text-4xl">Logo</h1>
          </section>
          <section className="flex justify-center items-center h-screen">
             <LoginForm />
